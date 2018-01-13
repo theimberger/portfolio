52963903d92bcf86f61eaa84abb0aab69ab7be49
theimberger <theimberger@gmail.com>
2018-01-12 19:28:44 -0500

    Add contact info

--- javascript/components/contact/contact.jsx
+++ javascript/components/contact/contact.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import pure from 'recompose/pure';
+import Profiles from './profiles.jsx';
 
 const Contact = (props) => {
   return (
-    <div>
-      this is a contact page
-    </div>
+    <React.Fragment>
+      <h2>contact</h2>
+      <hr />
+      <ul>
+        <li>
+          email
+          <hr />
+          tlheimberger@smcm.edu
+        </li>
+      </ul>
+    </ React.Fragment>
   );
 };
 
--- javascript/components/work/projects.jsx
+++ javascript/components/work/projects.jsx
@@ -3,10 +3,35 @@ import pure from 'recompose/pure';
 
 const Projects = (props) => {
   return (
-    <div>
+    <section id="projects_wrapper">
       <h2>projects</h2>
 
-    </div>
+      <section className="project_tile">
+        <h3>relax</h3>
+        <p>
+          A clone of the workplace chat application, Slack.
+          <br />Primary skills: Ruby/Rails, React/Redux
+        </p>
+      </section>
+
+      <section className="project_tile">
+        <h3>wikipedia crawler</h3>
+        <p>
+          A data visualization game that maps connections
+          between Wikipedia articles.
+          <br />Primary skills: Ruby/Rails, React/Redux
+        </p>
+      </section>
+
+      <section className="project_tile">
+        <h3>shiftFour</h3>
+      </section>
+
+      <section className="project_tile">
+        <h3>the name hall of fame</h3>
+      </section>
+
+    </section>
   );
 };
 
