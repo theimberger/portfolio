5cf3a679c3128cd4736c6c6e5a8aac318797f180
theimberger <theimberger@gmail.com>
2018-01-15 17:04:58 -0500

    Add icons for skills

--- javascript/components/experience/experience.jsx
+++ javascript/components/experience/experience.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Education from './education';
 import Employment from './employment';
+import Skills from './skills';
 import pure from 'recompose/pure';
 
 const Projects = (props) => {
   return (
     <React.Fragment>
-      <h2>education</h2>
+      <h2>/skills</h2>
+      <Skills />
+      <br />
+      <hr />
+      <h2>/education</h2>
       <Education />
       <hr />
-      <h2>employment</h2>
+      <h2>/employment</h2>
       <Employment />
     </React.Fragment>
   );
