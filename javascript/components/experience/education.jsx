cab158a3e7ecf161294741b0bb854d1def7907ee
theimberger <theimberger@gmail.com>
2018-01-15 16:16:59 -0500

    Add employment component

---
+++ javascript/components/experience/education.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import pure from 'recompose/pure';
+
+const Education = (props) => {
+  return (
+    <section className="wrapper experience">
+
+      <section className="tile">
+        <h3>App Academy</h3>
+        <p>
+          A 1,000+ hour coding intensive program, with an {"<"}2%
+          acceptance rate.
+        </p>
+        <p>
+          September 2017 - November 2017
+        </p>
+      </section>
+
+      <section className="tile">
+        <h3>St. Mary's College of Maryland</h3>
+        <p>
+          A small pubic honors college in southern Maryland.  Graduated with a
+          Bachelor of Arts (majoring in Biology and Philosophy).
+        </p>
+        <p>
+          August 2010 - May 2015
+        </p>
+      </section>
+
+      <section className="tile">
+        <h3>NOLS New Zealand</h3>
+        <p>
+          A program promoting wilderness and leadership skills.  Was elected
+          by my peers to lead an expidention in the Southern Alps of NZ.
+        </p>
+        <p>
+          April 2010 - June 2010
+        </p>
+      </section>
+      
+    </section>
+  );
+};
+
+export default pure(Education);
---
+++ javascript/components/experience/employment.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import pure from 'recompose/pure';
+
+const Employment = (props) => {
+  return (
+    <section className="wrapper experience">
+
+      <section className="tile">
+        <h3>Old Fox Books and Coffeehouse</h3>
+        <p>
+          Helped set up and create a web presence of Annapolis' local
+          bookstore, Old Fox Books.  Assisted in the creation of
+          a wordpress website.
+        </p>
+        <p>
+          October 2016 - August 2017
+        </p>
+      </section>
+
+      <section className="tile">
+        <h3>Dawson's Liqours</h3>
+        <p>
+          Helped manage and organize an inventory of more than 400 beers.
+          Provided customer servie and recommendations.
+        </p>
+        <p>
+          October 2015 - April 2016
+        </p>
+      </section>
+
+      <section className="tile">
+        <h3>Scribe America</h3>
+        <p>
+          Worked side-by-side with ER physicians, providing deep and
+          accurate patient records in high pressure environments while
+          maintaining a high quality of work
+        </p>
+        <p>
+          June 2014 - October 2015
+        </p>
+      </section>
+      
+    </section>
+  );
+};
+
+export default pure(Employment);
