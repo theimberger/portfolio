5f149db2a5a14a7929df6fbb32dafb46d56ff4a3
theimberger <theimberger@gmail.com>
2018-01-17 16:26:04 -0500

    Fix fonts

--- javascript/bundle.js
+++ javascript/bundle.js
@@ -1082,8 +1082,16 @@ var About = function About() {
     null,
     _react2.default.createElement(
       'p',
-      null,
-      'I\'m a 26 year old developer living in NYC (Bushwich at the moment).'
+      { style: { "textAlign": "left", "fontSize": "1em" } },
+      'I\'m a 26 year old developer living in NYC (Bushwick at the moment).  I love employing and learning new technologies to solve new and old problems. I\'m an avid learner and enjoy the challenge of mastering new systems and tools. The ability to create solutions and interpersonal connections through web development is something I find deeply gratifying.',
+      _react2.default.createElement('br', null),
+      _react2.default.createElement('br', null),
+      'A bit about me not realted to development - before I became a developer I was thru-hiking the Appalachian Trail and am a pretty outdoors-y guy in general.  I also enjoy baking, games, and good books.',
+      _react2.default.createElement('br', null),
+      _react2.default.createElement('br', null),
+      'I hope to help out on your next project,',
+      _react2.default.createElement('br', null),
+      '//t'
     )
   );
 };
--- javascript/components/other/about.jsx
+++ javascript/components/other/about.jsx
@@ -4,8 +4,21 @@ import pure from 'recompose/pure';
 const About = () => {
   return (
     <React.Fragment>
-      <p>
-        I'm a 26 year old developer living in NYC (Bushwich at the moment).
+      <p style={{"textAlign": "left", "fontSize": "1em"}}>
+        I'm a 26 year old developer living in NYC (Bushwick at the moment).  I
+        love employing and learning new technologies to solve new and old problems.
+        I'm an avid learner and enjoy the challenge of mastering new systems and
+        tools. The ability to create solutions and interpersonal connections through web
+        development is something I find deeply gratifying.
+        <br /><br />
+        A bit about me not realted to development -
+        before I became a developer I was thru-hiking the Appalachian Trail and
+        am a pretty outdoors-y guy in general.  I also enjoy baking, games, and
+        good books.
+
+        <br /><br />
+        I hope to help out on your next project,
+        <br />//t
       </p>
     </React.Fragment>
   );
