85314fd04801083f20a193808701b6bfa87dea81
theimberger <theimberger@gmail.com>
2018-01-19 19:04:05 -0500

    Styling

--- javascript/components/view_port.jsx
+++ javascript/components/view_port.jsx
@@ -7,7 +7,20 @@ const ViewPort = (props) => {
   }
 
   const scrollHandler = () => {
+    let headers = document.getElementsByTagName("h1"),
+        $path = $("#inner_path"),
+        i = 0;
+
+    let pt = parseInt($path.css("top"));
+    $path.css("top", pt - 1);
+    // $("#path").css("padding-top" + 1);
+    // while (i < headers.length) {
+    //   console.log(headers[i].offsetTop);
+    //   console.log(document.getElementById("view_port").scrollTop);
+    //   i ++;
+    // }
   };
+
   return (
     <div id="scroll_bar_fix">
       <div id="view_port" onScroll={scrollHandler}>
