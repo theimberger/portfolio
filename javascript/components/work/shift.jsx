c3fd18d66552cdbdaee443770e68dfed8fe905a1
theimberger <theimberger@gmail.com>
2018-01-25 16:27:55 -0500

    Add skeleton for project pages

---
+++ javascript/components/work/shift.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import pure from 'recompose/pure';
+
+const ShiftFour = (props) => {
+  return (
+    <div>
+      this is a project page
+    </div>
+  );
+};
+
+export default pure(ShiftFour);
