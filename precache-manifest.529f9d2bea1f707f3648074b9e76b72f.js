98a512c962c8cfa3f6133899bdcb72592fce1782
theimberger <theimberger@gmail.com>
2020-01-10 21:32:59 -0500

    Updates

---
+++ precache-manifest.529f9d2bea1f707f3648074b9e76b72f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe016621f9aa11c8f210f7c0f8d47d8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2fbd1285b196f39bd13",
+    "url": "/portfolio/static/css/main.66299b04.chunk.css"
+  },
+  {
+    "revision": "5a30f99db6d760ff36f9",
+    "url": "/portfolio/static/js/2.ac22e2bc.chunk.js"
+  },
+  {
+    "revision": "d2fbd1285b196f39bd13",
+    "url": "/portfolio/static/js/main.f5b4eebc.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "a196fe96ce65418165b2b4188f0b7d46",
+    "url": "/portfolio/static/media/color-game.a196fe96.png"
+  },
+  {
+    "revision": "9399243c1abb9deb504a3a49c145ca28",
+    "url": "/portfolio/static/media/nhof.9399243c.png"
+  },
+  {
+    "revision": "903de989960f3224ec8fba4f10829fcb",
+    "url": "/portfolio/static/media/relax.903de989.png"
+  },
+  {
+    "revision": "9951366e346a03f07891ce43f810eb43",
+    "url": "/portfolio/static/media/wikipedia-crawler.9951366e.png"
+  }
+]);
